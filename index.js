3f41ab87302b756632a2149adee0f4452bff3b32
Marq Roldan <hcmlroldan@gmail.com>
2023-06-12 11:48:17 +0800

    Register the incoming-call-activity component

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import Activity_IncomingCallScreen from './src/activities/IncomingCallScreen';
+
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent(
+  'incoming-call-activity',
+  () => Activity_IncomingCallScreen,
+);
